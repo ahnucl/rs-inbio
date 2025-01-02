a7b30b2451ded558615def176147e255c0063c24
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-01-02 09:51:51 -0300

    Cria componentes de ui

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant: "primary" | "secondary" | "ghost";
+}
+
+export function Button({ variant, className, ...props }: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        "p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
+        variant === "primary" && "bg-accent-purple",
+        variant === "secondary" && "bg-background-tertiary",
+        variant === "ghost" && "border-border-primary",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
