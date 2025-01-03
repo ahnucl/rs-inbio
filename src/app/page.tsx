1a57791a697f6e7126b61948ca6d3e96f0b89baf
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-01-03 11:49:42 -0300

    Cria Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import { Header } from "@/components/landing-page/header";
 import { Hero } from "../components/landing-page/hero";
 
 export default function Home() {
   return (
     <div className="max-w-7xl 2xl:max-w-[110rem] mx-auto">
       <Hero />
-      {/* <Header />
-    <VideoExplanation />
+      <Header />
+    {/* <VideoExplanation />
     <Pricing />
     <FAQ /> */}
     </div>
