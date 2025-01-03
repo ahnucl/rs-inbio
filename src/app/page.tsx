0beb0adbf325f82d3e8f6977e4220f87f86bf7b5
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-01-03 11:43:24 -0300

    Ajusta responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "../components/landing-page/hero";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="max-w-7xl 2xl:max-w-[110rem] mx-auto">
       <Hero />
       {/* <Header />
     <VideoExplanation />
