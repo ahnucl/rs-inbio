c15eda66cc17e240b6934e94a8570625590954b7
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-01-06 09:05:48 -0300

    Finaliza vídeo e preço

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Header } from "@/components/landing-page/header";
 import { Hero } from "../components/landing-page/hero";
+import { VideoExplanation } from "@/components/landing-page/video-explanation";
+import { Pricing } from "@/components/landing-page/pricing";
 
 export default function Home() {
   return (
     <div className="max-w-7xl 2xl:max-w-[110rem] mx-auto">
       <Hero />
       <Header />
-    {/* <VideoExplanation />
-    <Pricing />
-    <FAQ /> */}
+      <VideoExplanation />
+      <Pricing />
+    {/* <FAQ /> */}
     </div>
   );
 }
