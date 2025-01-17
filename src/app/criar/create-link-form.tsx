2609c0e355fd688f5a6b0afaa6f0e28120d0c859
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-01-17 13:22:58 -0300

    Criação de link

---
+++ src/app/criar/create-link-form.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { createLink } from "@/actions/create-link";
+import { verifyLink } from "@/actions/verify-link";
+import { Button } from "@/components/ui/button";
+import { TextInput } from "@/components/ui/text-input";
+import { sanitizeLink } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+export function CreateLInkForm() {
+  const router = useRouter();
+
+  const [link, setLink] = useState("");
+  const [error, setError] = useState("");
+
+  function handleLinkChange(e: ChangeEvent<HTMLInputElement>) {
+    setLink(sanitizeLink(e.target.value));
+    setError("");
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (link.length === 0) return setError("Escolha um link primeiro :)");
+
+    const isLinkTaken = await verifyLink(link);
+    if (isLinkTaken) return setError("Desculpe, esse link já está em uso.");
+
+    const isLinkCreated = await createLink(link);
+
+    if (!isLinkCreated)
+      return setError("Erro ao criar o perfil. Tente novamente.");
+
+    router.push(`/${link}`);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="w-full flex items-center gap-2">
+        <span className="text-white">projectinbio.com/</span>
+        <TextInput value={link} onChange={handleLinkChange} />
+        <Button className="w-[126px]">Criar</Button>
+      </form>
+      <div>
+        <span className="text-accent-pink">{error}</span>
+      </div>
+    </>
+  );
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import {ClassValue, clsx} from 'clsx'
-import {twMerge} from 'tailwind-merge'
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...classes: ClassValue[]) {
-  return twMerge(clsx(classes))
+  return twMerge(clsx(classes));
+}
+
+export function sanitizeLink(link?: string) {
+  if (!link) return "";
+
+  return link
+    .replace(/\s/g, "")
+    .replace(/[!@#$%^&*()_+\-=\[\]{};':"\\|,ˆ.<>\/?]+/, "")
+    .toLocaleLowerCase();
 }
