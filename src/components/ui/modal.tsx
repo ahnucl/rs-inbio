def5379aa11afc1ad79e94014e096d1f4296e5f9
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-01-20 16:03:28 -0300

    Cria componente de modal

---
+++ src/components/ui/modal.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import useOnClickOutside from '@/app/hooks/useOnClickOutside'
+import { useRef } from 'react'
+
+interface ModalProps {
+  children: React.ReactNode
+  isOpen: boolean
+  setIsOpen(isOpen: boolean): void
+  //   setIsOpen: (isOpen: boolean) => void
+}
+
+export function Modal({ children, isOpen, setIsOpen }: ModalProps) {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useOnClickOutside(ref, () => setIsOpen(false))
+
+  if (!isOpen) return null
+
+  return (
+    <div className="fixed inset-0 bg-[#787878]/10 flex items-center justify-center backdrop-blur-md z-50">
+      <div ref={ref}>{children}</div>
+    </div>
+  )
+}
