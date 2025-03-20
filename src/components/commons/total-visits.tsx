e6c2ccff79e5efe16a2a0299203da6b23186c8e3
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2025-03-20 16:27:50 -0300

    Cria portal da stripe  e permite ao usuário deslogar

--- src/components/commons/total-visits.tsx
+++ src/components/commons/total-visits.tsx
@@ -1,10 +1,19 @@
+import { manageAuth } from '@/actions/manage-auth'
+import { auth } from '@/lib/auth'
 import { TrendingUp } from 'lucide-react'
+import { PortalButton } from './porta-button'
 
 interface TotalVisitsProps {
   totalVisits?: number
+  showControls?: boolean
 }
 
-export function TotalVisits({ totalVisits = 0 }: TotalVisitsProps) {
+export async function TotalVisits({
+  totalVisits = 0,
+  showControls = false,
+}: TotalVisitsProps) {
+  const session = await auth()
+
   return (
     <div className="w-min whitespace-nowrap flex items-center gap-5 bg-background-secondary border border-border-primary px-8 py-3 rounded-xl shadow-lg">
       <span className="font-bold text-white">Total de visitas</span>
@@ -12,10 +21,14 @@ export function TotalVisits({ totalVisits = 0 }: TotalVisitsProps) {
         <span className="text-3xl font-bold">{totalVisits}</span>
         <TrendingUp />
       </div>
-      {/* <div className="flex items-center gap-2">
-        <button>Portal</button>
-        <button>Sair</button>
-      </div> */}
+      {showControls && (
+        <div className="flex items-center gap-2">
+          {session?.user.isSubscribed && <PortalButton />}
+          <form action={manageAuth}>
+            <button>Sair</button>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
